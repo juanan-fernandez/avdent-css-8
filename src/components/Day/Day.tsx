6b9a12dc99f1de3c0c9b29acfd988cdc04c6451d
juanan fernandez <juanan.fernandez@gmail.com>
2023-01-19 21:25:13 +0100

    Svg as components

--- src/components/Day/Day.tsx
+++ src/components/Day/Day.tsx
@@ -1,6 +1,10 @@
 import './Day.css';
 import Card from '../UI/Card/Card';
+import Low from '../UI/Low/Low';
 import { Weather } from '../../interfaces/weather';
+
+import low from '../../assets/low.svg';
+import precipitacion from '../../assets/precipitation.svg';
 
 interface Props {
 	weatherData: Weather;
@@ -17,10 +21,22 @@ const Day = ({ weatherData }: Props) => {
 			<p className='day__number'>{dayNumber}</p>
 			<Card backColor={getBackColor()} heightpx={450}>
 				<div className='day__weather'>
-					<img src='/images/partlycloudy.svg' alt='Partly Cloudy' />
-					<p>{temperature}</p>
-					<p>{rainChance}</p>
-					<p>{minTemperature}</p>
+					<img
+						className='day__weather--icon'
+						src='/images/partlycloudy.svg'
+						alt='Partly Cloudy'
+					/>
+					<p className='day__temperature--symbol'>o</p>
+					<p className='day__temperature'>{temperature}</p>
+					<p className='day__rainchance'>
+						<img src={precipitacion} className='day__rainchance--logo' />
+						{rainChance} %
+					</p>
+					<p className='day__rainchance'>
+						{/* <img src={low} className='day__rainchance--logo' /> */}
+						<Low classColorName='azul' />
+						{minTemperature}º
+					</p>
 				</div>
 			</Card>
 		</div>
